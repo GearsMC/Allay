4369774b19e14ca0927f34ead63f40a298c09b4f
ClexaGod <60543561+ClexaGod@users.noreply.github.com>
2026-09-02 18:06:40 +0000

    Ayarlar ekranindan gelen komutta da PlayerCommandEvent tetikle
    SettingsCommandPacketProcessor komutu olay tetiklemeden dogrudan
    calistiriyordu; ayni komut hangi pakete bindigine gore farkli davraniyor,
    komutu kesen eklentiler bu kutudan yazilani goremiyordu.
    
    GearsMC'de somut sonucu bir moderasyon atlatmasiydi: ozel mesaj kesicisi
    devreye girmedigi icin susturulmus bir oyuncu buradan /msg gonderebiliyordu.
    Artik CommandRequestPacketProcessor ile ayni akis.

--- server/src/main/java/org/allaymc/server/network/processor/ingame/SettingsCommandPacketProcessor.java
+++ server/src/main/java/org/allaymc/server/network/processor/ingame/SettingsCommandPacketProcessor.java
@@ -1,5 +1,6 @@
 package org.allaymc.server.network.processor.ingame;
 
+import org.allaymc.api.eventbus.event.player.PlayerCommandEvent;
 import org.allaymc.api.player.Player;
 import org.allaymc.api.registry.Registries;
 import org.allaymc.server.network.processor.PacketProcessor;
@@ -10,9 +11,24 @@ import org.cloudburstmc.protocol.bedrock.packet.SettingsCommandPacket;
  * @author daoge_cmd
  */
 public class SettingsCommandPacketProcessor extends PacketProcessor<SettingsCommandPacket> {
+    /**
+     * Ayarlar ekranindaki komut kutusundan gelen komutu isler.
+     *
+     * <p>{@code CommandRequestPacketProcessor} ile ayni sekilde {@link PlayerCommandEvent}
+     * tetiklenir. Onceden bu yol olayi atliyordu; oyuncudan gelen ayni komut hangi pakete
+     * bindigine gore farkli davraniyor, komutu kesen eklentiler bu kutudan yazilani
+     * goremiyordu. GearsMC'de bunun somut sonucu bir moderasyon atlatmasiydi: ozel mesaj
+     * kesicisi devreye girmedigi icin susturulmus bir oyuncu buradan {@code /msg}
+     * gonderebiliyordu.</p>
+     */
     @Override
     public void handleSync(Player player, SettingsCommandPacket packet, long receiveTime) {
-        Registries.COMMANDS.execute(player.getControlledEntity(), packet.getCommand().substring(1));
+        var command = packet.getCommand().substring(1);
+
+        var event = new PlayerCommandEvent(player.getControlledEntity(), command);
+        if (event.call()) {
+            Registries.COMMANDS.execute(player.getControlledEntity(), event.getCommand());
+        }
     }
 
     @Override
