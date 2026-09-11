c5cad3a17c86a067f461b1d640253752a82bebae
bonbionTR <bonbontrgaming@gmail.com>
2026-09-11 18:28:10 +0300

    hıhım

--- api/src/main/java/org/allaymc/api/eventbus/event/block/SignTextChangeEvent.java
+++ api/src/main/java/org/allaymc/api/eventbus/event/block/SignTextChangeEvent.java
@@ -20,14 +20,23 @@ public class SignTextChangeEvent extends BlockEvent implements CancellableEvent 
      */
     protected String[] text;
     /**
+     * The text of the edited face before this change.
+     */
+    protected String[] oldText;
+    /**
      * The player editing the sign.
      */
     protected EntityPlayer editor;
 
     public SignTextChangeEvent(Block block, String[] text, EntityPlayer editor) {
+        this(block, text, editor, new String[0]);
+    }
+
+    public SignTextChangeEvent(Block block, String[] text, EntityPlayer editor, String[] oldText) {
         super(block);
         setText(text);
         this.editor = editor;
+        this.oldText = oldText == null ? new String[0] : oldText.clone();
     }
 
     public void setText(String[] text) {
